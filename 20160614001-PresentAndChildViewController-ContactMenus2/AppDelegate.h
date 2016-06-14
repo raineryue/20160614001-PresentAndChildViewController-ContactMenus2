da99292ba251b8eedbbb0bfc9aec133f942db840
Rainer <it_jinlei.yue@163.com>
2016-06-14 15:29:21 +0800

    Initial Commit

---
+++ 20160614001-PresentAndChildViewController-ContactMenus2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160614001-PresentAndChildViewController-ContactMenus2
+//
+//  Created by Rainer on 16/6/14.
+//  Copyright © 2016年 Sand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160614001-PresentAndChildViewController-ContactMenus2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20160614001-PresentAndChildViewController-ContactMenus2
+//
+//  Created by Rainer on 16/6/14.
+//  Copyright © 2016年 Sand. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
